325a6ce48f9182cb0ba2fa16e59eb13f1c0eccad
abdul <abdul@DESKTOP-5E1I3ED>
2021-01-21 09:31:52 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PremiumCalc.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PremiumCalc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //Create and object of model, just to assign list of occupation
+            var viewCustModel = new CustomerModel();
+
+            //addding dummy data. this will come from WebAPI in real development
+            //The same i could achieve using viewbag as well but just trying to simulate close to real
+            viewCustModel.Occupations = GetOccupationList();
+
+            return View(viewCustModel);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        //Populate Occupation list with Hard coded values
+        private IEnumerable<Occupation> GetOccupationList()
+        {
+
+            return new List<Occupation>
+            {
+            new Occupation
+            {
+            OccupationName = "Cleaner", OccupationRating = "Light Manual"
+            }
+
+            , new Occupation
+            {
+            OccupationName = "Doctor",OccupationRating = "Professional"
+            }
+
+            , new Occupation
+            {
+            OccupationName = "Author", OccupationRating ="White Collar"
+            }
+
+              , new Occupation
+            {
+            OccupationName  = "Farmer", OccupationRating="Heavy Manual"
+            }
+
+                , new Occupation
+            {
+             OccupationName= "Mechanic", OccupationRating ="Heavy Manual"
+            }
+                  , new Occupation
+            {
+             OccupationName= "Florist", OccupationRating="Light Manual"
+            }
+                    , new Occupation
+            {
+            OccupationName = "Cleaner", OccupationRating ="Light Manual"
+            }
+                      , new Occupation
+            {
+            OccupationName = "Doctor", OccupationRating ="Professional"
+            }
+            }
+
+            ;
+        }
+
+    }
+}
---
+++ Models/CustomerModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PremiumCalc.Models
+{
+    public class CustomerModel
+    {
+        [Required(ErrorMessage = "Enter Customer Name")]
+        [Display(Name = "Customer Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter Date of Birth")]
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? Dob { get; set; }
+
+        [Required(ErrorMessage = "Enter your Age")]
+        [Display(Name = "Age")]
+        public Int16 Age { get; set; }
+
+        [Required(ErrorMessage = "Enter Occupation")]
+        [Display(Name = "Occupation")]
+        public string occupation { get; set; }
+
+        [Required(ErrorMessage = "Enter Sum Insured")]
+        [Display(Name = "Death- Sum Insured")]
+        [Column(TypeName = "numeric")]
+        public decimal DeathSumInsured { get; set; }
+
+        [Display(Name = "Premium Amount")]
+        public string PremiumAmount { get; set; }
+
+        public IEnumerable<Occupation> Occupations { get; set; }
+     
+    }
+}
